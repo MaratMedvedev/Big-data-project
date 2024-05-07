07a6cae71c47d2220a231b3252249e7d9c5e2028
Rail <r.sayfeev@yandex.ru>
2024-05-08 00:47:44 +0300

    Improve stage1

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,6 +1,9 @@
 # !/bin/bash
 python3 scripts/build_projectdb.py
 
+if $(hdfs dfs -test -d project/warehouse/property_details) ; then $(hdfs dfs -rm -r project/warehouse/property_details); fi
+if $(hdfs dfs -test -d project/warehouse/target_price_prediction) ; then $(hdfs dfs -rm -r project/warehouse/target_price_prediction); fi
+
 sqoop import-all-tables --connect jdbc:postgresql://hadoop-04.uni.innopolis.ru/team28_projectdb --username team28 \
 --password 6MBLFOY0lG3MNJTd --compression-codec=snappy --compress --as-avrodatafile --warehouse-dir=project/warehouse \
 --m 1
